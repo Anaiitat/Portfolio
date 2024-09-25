dce468480ea34b1c7dda500cbe60236e86a3b783
tatiana <tatiedu84@gmail.com>
2024-09-25 11:33:20 +0200

    creation et finition de presentation

---
+++ src/components/ThePresentation.vue
@@ -0,0 +1,43 @@
+<template>
+    <section id="presentation">
+        <h2>Présentation</h2>
+        <article>
+            <p>Bienvenue sur mon portfolio ! Je suis Tatiana ARNAUD, Développeuse Web
+            débutante de 27 ans <br />
+            avec une forte motivation et un grand intérêt pour le front et le back
+            end.<br /><br />
+            Malgré mon expérience récente, j'ai acquis de solides compétences en
+            HTML, CSS, JavaScript et j'ai déjà réalisé plusieurs projets <br />personnels
+            qui me permettent de mettre en avant mes capacités. <br /><br />
+            Dans ce portfolio, vous découvrirez quelques exemples de mes
+            réalisations,
+            <br />qui illustrent ma passion pour le développement web.
+            </p>
+        </article>
+    </section>
+</template>
+
+<style scoped>
+
+
+
+#presentation > article {
+    text-align: center;
+    padding: 0px 40px 40px 40px;
+    color:rgb(255, 255, 255);
+    text-shadow:1px 1px 2px rgba(0, 0, 0, 0.664) ;
+}
+
+
+h2{
+    font-size: 30px;
+    margin: 20px;
+    padding: 10px;
+    font-weight: bold;
+    color: rgba(112, 199, 255, 255);
+    text-shadow:1px 1px 2px rgb(0, 0, 0) ;
+    background-color: rgba(255, 255, 255, 0.863);
+    border: 1px, solid, rgba(112, 199, 255, 255);
+    border-radius: 10px;
+}
+</style>
